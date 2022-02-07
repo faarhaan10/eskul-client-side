52158a306381276dd30959826bbf2fef63224f81
Farhan Elias <faarhaan10@gmail.com>
2022-02-07 14:23:45 +0600

    first commit

---
+++ src/Components/Shared/NavigationBar/NavigationBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavigationBar = () => {
+    return (
+        <div>
+            <h1>this is navigation bar/ menu/ appbar</h1>
+        </div>
+    );
+};
+
+export default NavigationBar;
